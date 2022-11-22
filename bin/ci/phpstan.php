370b490c7cb394f34bb054f4998432e6ba7d176c
Steevan BARBOYON <steevan.barboyon@gmail.com>
2022-11-22 16:09:19 +0100

    Add compatibility with PHP 8.1

--- bin/ci/phpstan.php
+++ bin/ci/phpstan.php
@@ -15,7 +15,7 @@ require dirname(__DIR__, 2) . '/vendor/autoload.php';
 
 function createPhpstanProcesses(string $phpVersion = null): ProcessArray
 {
-    $phpVersions = is_string($phpVersion) ? [$phpVersion] : ['7.4', '8.0'];
+    $phpVersions = is_string($phpVersion) ? [$phpVersion] : ['7.4', '8.0', '8.1'];
 
     $return = new ProcessArray();
     foreach ($phpVersions as $loopPhpVersion) {
